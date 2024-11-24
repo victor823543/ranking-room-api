5d1b50da73c348aad1a6ca715f96c47b6b459608
Victor <v.safsten@gmail.com>
2024-11-24 17:57:40 +0100

    Add cicd pipeline

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,6 @@ export const MONGO_URI =
   NODE_ENV === "devtest"
     ? process.env.MONGO_URI_TEST || ""
     : process.env.MONGO_URI || "";
-export const API_ADDRESS = process.env.API_ADDRESS || "";
 export const PORT = "4000";
 export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "";
 // export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID || "";
